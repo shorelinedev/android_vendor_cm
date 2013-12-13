2955034bedba70a67f10a31f567173b28cf50188
Matthew <m.hanhimaki@gmail.com>
2013-12-13 16:22:35 -0500

    added ViPER app flag for build error

--- config/shore_audio.mk
+++ config/shore_audio.mk
@@ -1,3 +1,4 @@
 # This device supports ShoreDroid Audio Enhancements
 PRODUCT_COPY_FILES += \
+    vendor/cm/config/ShoreAudio/ViPER4Android_4.x_2315.apk:system/app/ViPER4Android_4.x_2315.apk \
     vendor/cm/config/ShoreAudio/libv4a_fx_jb_NEON.so:system/lib/soundfx/libv4a_fx_jb_NEON.so
