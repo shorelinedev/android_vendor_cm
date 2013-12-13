6f426d114a725e2413701c9a6fd55d73bd620295
Matthew <m.hanhimaki@gmail.com>
2013-12-12 20:07:55 -0500

    added template to copy over audio libs for ViPER

--- config/shore_audio.mk
+++ config/shore_audio.mk
@@ -1,1 +1,3 @@
-Coming Soon!
+# This device supports ShoreDroid Audio Enhancements
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
