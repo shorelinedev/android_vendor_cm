166b074ba0030edf03b7b815eee500583b2f4379
Matthew <m.hanhimaki@gmail.com>
2013-12-13 18:42:20 -0500

    removed for redundancy

--- config/shore_audio.mk
+++
@@ -1,4 +0,0 @@
-# This device supports ShoreDroid Audio Enhancements
-PRODUCT_COPY_FILES += \
-    vendor/cm/config/ShoreAudio/ViPER4Android_4.x_2315.apk:system/app/ViPER4Android_4.x_2315.apk \
-    vendor/cm/config/ShoreAudio/libv4a_fx_jb_NEON.so:system/lib/soundfx/libv4a_fx_jb_NEON.so
