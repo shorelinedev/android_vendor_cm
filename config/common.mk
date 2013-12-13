d89fc41df04c259895510017e5d159e30048cd0c
Matthew <m.hanhimaki@gmail.com>
2013-12-13 18:47:33 -0500

    fixed some spacing

--- config/common.mk
+++ config/common.mk
@@ -136,7 +136,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
     
 # ViPER Audio
-PRODUCT_COPY_FILES +=  \
+PRODUCT_COPY_FILES += \
     vendor/cm/config/ShoreAudio/ViPER4Android_4.x_2315.apk:system/app/ViPER4Android_4.x_2315.apk \
     vendor/cm/config/ShoreAudio/libv4a_fx_jb_NEON.so:system/lib/soundfx/libv4a_fx_jb_NEON.so
 
